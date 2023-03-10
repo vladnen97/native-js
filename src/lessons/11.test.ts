b76d93ca91e87cf762690acbf3998dfa54fd62de
vladnen97 <darkdescent1991@gmail.com>
2023-03-10 06:36:35 +0300

    lesson 11 exercises: more than 100 points

---
+++ src/lessons/11.test.ts
@@ -0,0 +1,57 @@
+type studentType = {
+    name: string
+    age: number
+    isMarried: boolean
+    scores: number
+}
+
+let studentsArray2: Array<studentType>;
+
+beforeEach(() => {
+    studentsArray2 = [
+        {
+            name: "Bob",
+            age: 22,
+            isMarried: true,
+            scores: 85,
+        },
+        {
+            name: "Alex",
+            age: 21,
+            isMarried: true,
+            scores: 90,
+        },
+        {
+            name: "Nick",
+            age: 20,
+            isMarried: false,
+            scores: 120,
+        },
+        {
+            name: "John",
+            age: 19,
+            isMarried: false,
+            scores: 100,
+        },
+        {
+            name: "Helen",
+            age: 20,
+            isMarried: false,
+            scores: 110,
+        },
+        {
+            name: "Ann",
+            age: 20,
+            isMarried: false,
+            scores: 105,
+        },
+    ];
+})
+
+test('Сформируйте массив студентов, у которых 100 и более баллов', () => {
+
+    const moreThen100 = studentsArray2.filter(el => el.scores >= 100)
+
+    expect(moreThen100.length).toBe(4)
+
+})
